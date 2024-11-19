b1397d7bd48bb388ebb33ad4e5571350e1622df2
SerenityScript <yuliasiebrandt89@gmail.com>
2024-11-19 22:39:44 +0100

    removed behavior:smooth in the button

---
+++ src/Components/BtnUp/BtnUp.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+import './styleBtnUp.css';
+import arrowUp from './up-arrow.png';
+
+const BtnUp = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 500) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+
+
+    <div>
+      
+      {isVisible && ( <button onClick={() => window.scrollTo({ top: 0})} className={`BtnUp ${isVisible ? "visible" : ""}`}>
+        <img src={arrowUp} className='BtnUpIcon' alt='Arrow up' />
+      </button>)}
+
+    </div>
+  )
+}
+
+export default BtnUp;
