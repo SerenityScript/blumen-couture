837b1b8d581c2b2c09c177f093c084231483ab12
SerenityScript <yuliasiebrandt89@gmail.com>
2024-11-27 09:42:26 +0100

    modified src/Components/Tabs/TabInfoOne.js

--- src/Components/Tabs/TabInfoOne.js
+++ src/Components/Tabs/TabInfoOne.js
@@ -1,6 +1,6 @@
 const TabInfoOne = () => {
   return (
-    <div className="HeadingCont">
+    <div className="HeadingCont TabsMainContainer">
 
       <div className="TabsCont">
         <div className="TabImgCont">
