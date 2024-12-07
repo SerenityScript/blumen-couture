f8ccbda63011d341518744c013d0441447545293
SerenityScript <yuliasiebrandt89@gmail.com>
2024-12-07 22:09:08 +0100

    code clean

--- src/Components/Instagram/Instagram.js
+++ src/Components/Instagram/Instagram.js
@@ -1,3 +1,5 @@
+import instagramData from './instagramData';
+import InstaImage from './InstaImage';
 import './styleInstagram.css';
 
 const Instagram = () => {
@@ -7,55 +9,9 @@ const Instagram = () => {
         <h3 className="HeadingGold">INSTAGRAM</h3>
       
         <div className="InstaFeed">
-        
-          <div className="InstaImgCont">
-            <img
-              src="https://images.unsplash.com/photo-1525772764200-be829a350797?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEzNXx8fGVufDB8fHx8fA%3D%3D"
-              alt="Table with beautiful decoration"
-              className="InstaImg"
-            />
-          </div>
-        
-          <div className="InstaImgCont">
-            <img
-              src="https://images.unsplash.com/photo-1521543832500-49e69fb2bea2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGJsdW1lbnN0cmF1c3N8ZW58MHx8MHx8fDA%3D"
-              alt="Wedding flower bouquet"
-              className="InstaImg"
-            />
-          </div>
-        
-          <div className="InstaImgCont">
-            <img
-              src="https://plus.unsplash.com/premium_photo-1661775142165-0c85050144fd?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE0NXx8fGVufDB8fHx8fA%3D%3D"
-              alt="Luxury decorated table with flowers"
-              className="InstaImg"
-            />
-          </div>
-          </div>
-          <div className="InstaFeed">
-          <div className="InstaImgCont">
-            <img
-              src="https://images.unsplash.com/photo-1503652601-557d07733ddc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDJ8fHxlbnwwfHx8fHw%3D"
-              alt="Beautiful flowers in a picknic corb"
-              className="InstaImg"
-            />
-          </div>
-          
-          <div className="InstaImgCont">
-            <img
-              src="https://images.unsplash.com/photo-1525441638700-20f9d4270dc7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDExfHx8ZW58MHx8fHx8"
-              alt="Luxury flower bouquets"
-              className="InstaImg"
-            />
-          </div>
-        
-          <div className="InstaImgCont">
-            <img
-              src="https://images.unsplash.com/photo-1591886960571-74d43a9d4166?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEwfHx8ZW58MHx8fHx8"
-              alt="Beautiful roses"
-              className="InstaImg"
-            />
-          </div>
+          {instagramData.map((image) => (
+            <InstaImage key={image.id} src={image.src} alt={image.alt} />
+          ))}
         </div>
       
         <p className="ParGold">@blumen_couture</p>
