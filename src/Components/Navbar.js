ad9d3a0c75308b3d62a43200588a131749d9529d
SerenityScript <yuliasiebrandt89@gmail.com>
2024-12-08 15:31:41 +0100

    Routes deleted from navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,12 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { Routes, Route, Link, useLocation } from 'react-router-dom';
-import Home from '../Pages/Home';
-import EventDecoration from '../Pages/EventDecoration';
-import Shop from '../Pages/Shop/Shop';
-import About from '../Pages/About';
-import Contact from '../Pages/Contact';
+import { Link, useLocation } from 'react-router-dom';
 import logo from '../Assets/logo.png';
-import AboutProducts from '../Pages/Shop/AboutProducts';
 import { gsap } from 'gsap';
 
 const Navbar = () => {
@@ -110,15 +104,6 @@ const Navbar = () => {
           </Link>
         </div>
       </nav>
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/eventDecoration" element={<EventDecoration />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/about/:title" element={<AboutProducts />} />
-      </Routes>
     </div>
   )
 }
