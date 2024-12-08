c869d3492e622ab3bb6d7e43f43969d70c68c0a0
SerenityScript <yuliasiebrandt89@gmail.com>
2024-12-08 15:32:22 +0100

    RoutesConfig component added to App.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,15 @@ import React from 'react';
 import './App.css';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer/Footer';
+import RoutesConfig from './Routes/RoutesConfig';
 
 function App() {
 
   return (
     <div className="App">
-
       <Navbar />
-
+      <RoutesConfig />
       <Footer />
-      
     </div>
   );
 }
