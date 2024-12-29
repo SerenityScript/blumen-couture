e3e3bd31c17fb4b2dd73181e67e3c9dcc42703ec
SerenityScript <yuliasiebrandt89@gmail.com>
2024-12-29 13:52:04 +0100

    took data out

--- src/Components/Tabs/RomanticTab.js
+++ src/Components/Tabs/RomanticTab.js
@@ -1,24 +1,8 @@
+import { imagesRomanticTab } from "../../data/data";
 import TabImages from "./TabImages";
 
 const RomanticTab = () => {
-  const images = [
-    {
-      src: "https://images.unsplash.com/photo-1495705554858-ca4a9a34ad6f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Ym91cXVldCUyMHdlZGRpbmclMjBibHVlfGVufDB8fDB8fHww",
-      alt: "Flowers 1",
-    },
-    {
-      src: "https://images.unsplash.com/photo-1558663876-3a629e2ff015?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTJ8fHdlZGRpbmclMjBjYWtlfGVufDB8fDB8fHww",
-      alt: "Flowers 2",
-    },
-    {
-      src: "https://images.unsplash.com/photo-1719223852076-6981754ebf76?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDM3fHx8ZW58MHx8fHx8",
-      alt: "Flowers 3",
-    },
-    {
-      src: "https://images.unsplash.com/photo-1471696035578-3d8c78d99684?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8aG9ydGVuc2llfGVufDB8fDB8fHww",
-      alt: "Flowers 4",
-    },
-  ];
+  const images = imagesRomanticTab;
 
   return (
     <div className="HeadingCont TabsMainContainer">
